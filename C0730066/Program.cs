74c692e52ddce8b92f6328740bf4e5bb088d1b5d
visualstudio <visualstudio@122-PC04>
2019-04-04 18:54:19 -0400

    Commit

--- C0730066/Program.cs
+++ C0730066/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace C0730066
@@ -10,6 +11,22 @@ namespace C0730066
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Peter Rules the IT Universe!");
+            Download();
+            Console.ReadLine();
+        }
+
+        static async void Download()
+        {
+            await Network.Download();
+            Console.WriteLine("Download Complete");
+        }
+        class Network
+        {
+            static public Task Download()
+            {
+                return Task.Run(() => Thread.Sleep(3000));
+            }
         }
     }
 }
